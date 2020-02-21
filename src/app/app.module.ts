02b6fd3f4fb0e48181ce270982ca6b6c561ec90b
Beatons <studentas.nerijus@gmail.com>
2020-02-21 19:32:05 +0100

    added hmr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
+import { NgModule, ApplicationRef } from '@angular/core'
 
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
@@ -7,18 +7,32 @@ import { MatCardModule } from '@angular/material/card'
 import { MatIconModule } from '@angular/material/icon'
 import { FlexLayoutModule } from '@angular/flex-layout'
 import { TimelineModule } from './components/timeline/timeline.module'
-
+import { StateContext } from '@ngxs/store'
+import { NgxsHmrLifeCycle, NgxsHmrSnapshot as Snapshot } from '@ngxs/hmr-plugin'
 @NgModule({
-    declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        MatCardModule,
-        MatIconModule,
-        FlexLayoutModule,
-        TimelineModule,
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatIconModule,
+    FlexLayoutModule,
+    TimelineModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule implements NgxsHmrLifeCycle<Snapshot> {
+  public hmrNgxsStoreOnInit(
+    ctx: StateContext<Snapshot>,
+    snapshot: Partial<Snapshot>
+  ) {
+    ctx.patchState(snapshot)
+  }
+
+  public hmrNgxsStoreBeforeOnDestroy(
+    ctx: StateContext<Snapshot>
+  ): Partial<Snapshot> {
+    return ctx.getState()
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
+import { enableProdMode, NgModuleRef } from '@angular/core'
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppModule } from './app/app.module'
+import { environment } from './environments/environment'
 
 if (environment.production) {
-  enableProdMode();
+  enableProdMode()
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule)
+
+if (environment.hmr) {
+  import('@ngxs/hmr-plugin').then(plugin => {
+    plugin.hmr(module, bootstrap).catch((err: Error) => console.error(err))
+  })
+} else {
+  bootstrap().catch((err: Error) => console.log(err))
+}
